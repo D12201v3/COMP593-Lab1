96e5d42f907e9ad69bb3da602d6c1d6f7f39f4b3
D12201v3 <Hayden.Wood@flemingcollege.ca>
2022-06-02 18:09:31 -0400

    Completed new feature
    added the best feature that has ever existed

--- Script1.py
+++ Script1.py
@@ -1,2 +1,4 @@
 ## Printing Lines of Zen of Python (Line 1 and 2)
 print ("Beautiful is better than ugly.")
+
+## Very epic new feature this feature could cure cancer or solve would hunger it is such a good and awsome feature
