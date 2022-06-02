8d254f3dfa25623a7e8183d4bab9cdef7636c58b
D12201v3 <Hayden.Wood@flemingcollege.ca>
2022-06-02 17:52:04 -0400

    Revert "Changed Both Scripts"
    This reverts commit 4f3105d7732cc12e4f84e1bd90677e46d8a2bb34.

--- Script1.py
+++ Script1.py
@@ -1,2 +1,3 @@
 ## Printing Lines of Zen of Python (Line 1 and 2)
 print ("Beautiful is better than ugly.")
+print ("Explicit is better than implicit.")
--- Script2.py
+++ Script2.py
@@ -1,3 +1,2 @@
-## 2 ## are better then one (Lines 3 and 4 of Zen of Python)
 print ("Simple is better than complex.")
 print ("Complex is better than complicated.")
