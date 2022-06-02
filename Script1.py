268ef2036d7380f7194389fa795848efaa9d9cf5
D12201v3 <Hayden.Wood@flemingcollege.ca>
2022-06-02 18:02:49 -0400

    Changed both Files again
    -Removed print
    -Updated comment and added Print

--- Script1.py
+++ Script1.py
@@ -1,3 +1,2 @@
 ## Printing Lines of Zen of Python (Line 1 and 2)
 print ("Beautiful is better than ugly.")
-print ("Explicit is better than implicit.")
--- Script2.py
+++ Script2.py
@@ -1,3 +1,4 @@
-## 2 ## are better then one (Lines 3 and 4 of Zen of Python)
+## 2 ## are better then one (Lines 3 and 4 of Zen of Python) (added line 5)
 print ("Simple is better than complex.")
 print ("Complex is better than complicated.")
+print ("Flat is better than nested.")
